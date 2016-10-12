590f8262708fc4919a65a9322af0c5892f54cbcc
Giovanna <g.c1@me.com>
2016-10-11 20:28:27 -0500

    Giovanna's app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/:message', function(request, response, next) {
+    const message = request.params.message;
+    console.log(request);
+    return response.json(message);
+});
+
+const port = 3000;
+
+app.listen(port, function () {
+    console.log('Listening on port: ${port}');
+});
